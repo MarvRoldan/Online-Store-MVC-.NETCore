03a0844de6df2e7f11924668540636270b396833
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-18 13:42:45 -0600

    Initial commit

---
+++ Online-Store/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Store.Controllers
+{
+    public class StoreController : Controller
+    {
+        public string Index()
+        {
+            return "Index";
+        }
+        public string Browse()
+        {
+            return "Browse";
+        }
+        public string Details()
+        {
+            return "Details";
+        }
+    }
+}
